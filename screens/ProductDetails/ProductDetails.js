1aa76137c3b1ebaaaedda01be9e87ec6db7d7907
Mennaakram <mennaakram976@gmail.com>
2024-04-26 20:10:30 +0300

    handle back button

--- screens/ProductDetails/ProductDetails.js
+++ screens/ProductDetails/ProductDetails.js
@@ -10,37 +10,39 @@ import Button from "../../components/Button";
 import SelectColor from "./components/SelectColor";
 import Details from "./components/Details";
 import { Appbar } from "react-native-paper";
-import { useRouter } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 import Specifications from "./components/Specifications";
 import ProductReviews from "./components/ProductReviews";
 
 export default function ProductDetails() {
   const router = useRouter();
+  const navigation = useNavigation();
   
   return (
     <SafeAreaView style={{marginBottom: 24}}>
       <StatusBar />
       <FlatList
-    showsVerticalScrollIndicator={false}
-    data={[{}]}
-    renderItem={({item}) => <>
-        <Appbar.Header>
-          <Appbar.BackAction onPress={() => router.back()} />
-          <Appbar.Content title="Nike Air Zoom Pegasus 36 Miami"/>
-          <Appbar.Action icon="magnify" onPress={() => {}} />
-          <Appbar.Action icon="dots-vertical" onPress={() => {}}/>
-        </Appbar.Header>
-        <Details />
-        <SelectSize />
-        <SelectColor />
-        <Specifications/>
-        <ProductReviews/>
-        <Recomended />
-        <Button text="Add to cart" />
-      </>
-      }
-    ItemSeparatorComponent={() => <View style={{height: 16}}/>}
-    />
+        showsVerticalScrollIndicator={false}
+        data={[{}]}
+        renderItem={({item}) => (
+          <>
+            <Appbar.Header>
+              <Appbar.BackAction onPress={() => router.replace(herf=navigation.goBack)} />
+              <Appbar.Content title="Nike Air Zoom Pegasus 36 Miami" />
+              <Appbar.Action icon="magnify" onPress={() => {}} />
+              <Appbar.Action icon="dots-vertical" onPress={() => {}} />
+            </Appbar.Header>
+            <Details />
+            <SelectSize />
+            <SelectColor />
+            <Specifications />
+            <ProductReviews />
+            <Recomended />
+            <Button text="Add to cart" />
+          </>
+        )}
+        ItemSeparatorComponent={() => <View style={{height: 16}} />}
+      />
     </SafeAreaView>
   );
 }
