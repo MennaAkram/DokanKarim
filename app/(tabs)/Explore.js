371eac2fda62f00d177fc85537eef8e98168877f
Ahmed-fouad2210 <150384292+Ahmed-fouad2210@users.noreply.github.com>
2024-04-29 17:42:04 +0300

    Search0
    Search With Filter ( UI needs edits & Sort Menu)

--- app/(tabs)/Explore.js
+++ app/(tabs)/Explore.js
@@ -1,11 +1,103 @@
-import { useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
 
-export default function User(){
-    const router = useRouter();
+export default function User() {
+  const [filterData, setFilterData] = useState([]);
+  const [masterData, setMasterData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = () => {
+    const apiURL = "https://fakestoreapi.com/products";
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((responsejson) => {
+        setFilterData(responsejson);
+        setMasterData(responsejson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const searchFilter = (text) => {
+    if (text) {
+      const newData = masterData.filter((item) => {
+        const itemData = item.title ? item.title.toUpperCase() : "";
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilterData(newData);
+      setSearch(text);
+    } else {
+      setFilterData(masterData);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
     return (
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Text>Explore</Text>
-        </View>
-    )
+      <Text style={styles.itemStyle}>
+        {item.id}
+        {". "}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      <View
+        style={{ height: 0.5, width: "100%", backgroundColor: "#c8c8c8" }}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          value={search}
+          placeholder="Search Here "
+          onChangeText={searchFilter}
+        />
+        <FlatList
+          data={filterData}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+  },
+  itemStyle: {
+    padding: 15,
+  },
+  textInputStyle: {
+    height: 50,
+    borderWidth: 1,
+    borderBlockColor: "blue",
+    paddingLeft: 20,
+    margin: 15,
+    borderColor: "#009688",
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+});
