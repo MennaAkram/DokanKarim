fe282732f2d9042a5129853388ac8ccb54161aa4
Ahmed-fouad2210 <150384292+Ahmed-fouad2210@users.noreply.github.com>
2024-05-10 18:42:08 +0300

    Search Almost Done !

--- app/(tabs)/Explore.js
+++ app/(tabs)/Explore.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   StyleSheet,
   SafeAreaView,
@@ -6,77 +6,306 @@ import {
   Text,
   View,
   TextInput,
+  TouchableOpacity,
+  Modal,
+  Image,
 } from "react-native";
-import SearchBar from "../../components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function User() {
-  const [filterData, setFilterData] = useState([]);
-  const [masterData, setMasterData] = useState([]);
-  const [filter, setFilter] = useState(true);
+  const router = useRouter();
+  const [visible, setVisible] = useState(false);
+  const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-
+  const searchRef = useRef();
+  const listRef = useRef();
+  const [ind, setInd] = useState(0);
+  const [oldData, setOldData] = useState([]);
   useEffect(() => {
-    fetchItems();
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        setData(response);
+        setOldData(response);
+      });
   }, []);
 
-  const fetchItems = () => {
-    const apiURL = "https://fakestoreapi.com/products";
-    fetch(apiURL)
-      .then((response) => response.json())
-      .then((responsejson) => {
-        setFilterData(responsejson);
-        setMasterData(responsejson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const searchFilter = (text) => {
-    if (text) {
-      const newData = masterData.filter((item) => {
-        const itemData = item.title ? item.title.toUpperCase() : "";
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilterData(newData);
-      setSearch(text);
+  const onSearch = (text) => {
+    if (text == "") {
+      setData(oldData);
     } else {
-      setFilterData(masterData);
-      setSearch(text);
+      let tempList = data.filter((item) => {
+        return item.title.toLowerCase().indexOf(text.toLowerCase()) > -1;
+      });
+      setData(tempList);
     }
   };
 
-  const ItemView = ({ item }) => {
-    return (
-      <Text style={styles.itemStyle}>
-        {item.id}
-        {". "}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
+  const onSortPress = (sortType) => {
+    switch (sortType) {
+      case "name":
+        setData(data.sort((a, b) => (a.title > b.title ? 1 : -1)));
+        break;
+      case "price-low-to-high":
+        setData(data.sort((a, b) => a.price - b.price));
+        break;
+      case "price-high-to-low":
+        setData(data.sort((a, b) => b.price - a.price));
+        break;
+      case "rating":
+        setData(data.sort((a, b) => b.rating.rate - a.rating.rate));
+        break;
+      default:
+        break;
+    }
 
-  const ItemSeparatorView = () => {
-    return (
-      <View
-        style={{ height: 0.5, width: "100%", backgroundColor: "#c8c8c8" }}
-      />
-    );
+    listRef.current.scrollToIndex({
+      animated: true,
+      index: 0,
+    });
+
+    setVisible(false);
   };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <SearchBar />
       <View style={styles.container}>
-        {(filter === true && true) || (
-          <FlatList
-            data={filterData}
-            keyExtractor={(item, index) => index.toString()}
-            ItemSeparatorComponent={ItemSeparatorView}
-            renderItem={ItemView}
-          />
-        )}
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+            height: 70,
+            marginTop: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          <View
+            style={{
+              width: "80%",
+              height: 50,
+              borderRadius: 10,
+              borderWidth: 0.2,
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: 15,
+            }}
+          >
+            <Image
+              source={require("../../assets/icons/Search.png")}
+              style={{ width: 24, height: 24, marginLeft: 15, opacity: 0.5 }}
+            />
+            <TextInput
+              ref={searchRef}
+              placeholder="search item here ... "
+              style={{ width: "76%", height: 50 }}
+              value={search}
+              onChangeText={(txt) => {
+                onSearch(txt);
+                setSearch(txt);
+              }}
+            />
+            {search == "" ? null : (
+              <TouchableOpacity
+                style={{ marginRight: 15 }}
+                onPress={() => {
+                  searchRef.current.clear();
+                  onSearch(""), setSearch("");
+                }}
+              >
+                <Image
+                  source={require("../../assets/icons/Left.png")}
+                  style={{ width: 16, height: 16, opacity: 0.5 }}
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+          <TouchableOpacity
+            style={{
+              marginRight: 15,
+            }}
+            onPress={() => {
+              setVisible(true);
+            }}
+          >
+            <Image
+              source={require("../../assets/icons/love.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={data}
+          ref={listRef}
+          showsVerticalScrollIndicator={false}
+          initialScrollIndex={ind}
+          renderItem={({ item, index }) => {
+            return (
+              <View
+                style={{
+                  width: "90%",
+                  borderRadius: 10,
+                  borderWidth: 0.5,
+                  alignSelf: "center",
+                  marginTop: 20,
+                  marginBottom: index == data.length - 1 ? 20 : 0,
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Image
+                  source={{ uri: item.image }}
+                  style={{
+                    width: 60,
+                    height: "90%",
+                    marginLeft: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <View style={{ width: "80%" }}>
+                  <Text
+                    style={{ fontWeight: "600", marginLeft: 10, marginTop: 10 }}
+                  >
+                    {item.title.substring(0, 30)}
+                  </Text>
+                  <Text style={{ fontSize: 12, margin: 10 }}>
+                    {item.description.substring(0, 50)}
+                  </Text>
+
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      marginBottom: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 18,
+                        marginLeft: 10,
+                        fontWeight: "800",
+                        color: "green",
+                      }}
+                    >
+                      {"$ " + item.price}
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        marginLeft: 50,
+                        fontWeight: "800",
+                        color: "orange",
+                      }}
+                    >
+                      {item.rating.rate}
+                    </Text>
+                    <Image
+                      source={require("../../assets/icons/love.png")}
+                      style={{ width: 12, height: 12, marginLeft: 5 }}
+                    />
+                  </View>
+                </View>
+              </View>
+            );
+          }}
+        />
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={visible}
+          onRequestClose={() => {
+            setVisible(!visible);
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(0,0,0,.5)",
+            }}
+          >
+            <View
+              style={{
+                width: "80%",
+                height: 200,
+                borderRadius: 10,
+                backgroundColor: "#fff",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  width: "100%",
+                  height: 50,
+                  borderBottomWidth: 0.5,
+                  justifyContent: "center",
+                  paddingLeft: 20,
+                }}
+                onPress={() => {
+                  onSortPress("name");
+                }}
+              >
+                <Text style={{ fontSize: 18, color: "#000" }}>
+                  Sort By Name
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  width: "100%",
+                  height: 50,
+                  borderBottomWidth: 0.5,
+                  justifyContent: "center",
+                  paddingLeft: 20,
+                }}
+                onPress={() => {
+                  onSortPress("price-low-to-high");
+                }}
+              >
+                <Text style={{ fontSize: 18, color: "#000" }}>
+                  Low to High Price
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  width: "100%",
+                  height: 50,
+                  borderBottomWidth: 0.5,
+                  justifyContent: "center",
+                  paddingLeft: 20,
+                }}
+                onPress={() => {
+                  onSortPress("price-high-to-low");
+                }}
+              >
+                <Text style={{ fontSize: 18, color: "#000" }}>
+                  Hight to Low Price
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  width: "100%",
+                  height: 50,
+                  borderBottomWidth: 0.5,
+                  justifyContent: "center",
+                  paddingLeft: 20,
+                }}
+                onPress={() => {
+                  onSortPress("rating");
+                }}
+              >
+                <Text style={{ fontSize: 18, color: "#000" }}>
+                  Sort By Rating
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     </SafeAreaView>
   );
@@ -85,6 +314,7 @@ export default function User() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
+    flex: 1,
   },
   itemStyle: {
     padding: 15,
@@ -98,5 +328,16 @@ const styles = StyleSheet.create({
     borderColor: "#009688",
     backgroundColor: "white",
     borderRadius: 10,
+  },
+  counterText: {
+    alignSelf: "center",
+    marginVertical: 10,
+    fontWeight: "bold",
+  },
+  noItemText: {
+    alignSelf: "center",
+    marginVertical: 10,
+    fontWeight: "bold",
+    color: "red",
   },
 });
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,46 +1,63 @@
 import React from "react";
-import { Colors } from "../../../constants/Colors";
-import { Image, TextInput, View, StyleSheet } from "react-native-web";
+//import {  } from "../../../constants/";
+import { Image, TextInput, View, StyleSheet, Pressable } from "react-native";
 
-export default function SearchBar() {
+export default function SearchBar({ isFilter, onPress, text, onChangeText }) {
   return (
-    <View style={StyleSheet.container}>
-      <View style={StyleSheet.container}>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.SearchBar}
+        placeholder="Search"
+        value={text}
+        onChangeText={onChangeText}
+      />
+
+      <Pressable onPress={onPress}>
         <Image
-          source={require("../../../assets/icons/search.png")}
-          style={styles.image}
+          source={
+            isFilter
+              ? require("../assets/icons/FilledLove.png")
+              : require("../assets/icons/love.png")
+          }
         />
-        <TextInput
-          placeholder="Search"
-          placeholderTextColor={Colors.greyText}
-          value=""
-          onChangeText={() => {}}
-        />
-      </View>
-      <Image source={require("../../../assets/icons/love.png")} />
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     marginHorizontal: 16,
   },
   SearchBar: {
+    paddingLeft: "3%",
+
+    backgroundColor: "#ffff",
     width: "90%",
     flexDirection: "row",
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 15,
     height: 46,
-    borderColor: Colors.border,
+    borderColor: "gray",
   },
   image: {
     width: 16,
     height: 16,
     padding: 10,
     margin: 10,
+  },
+  textInputStyle: {
+    height: 50,
+    borderWidth: 1,
+    borderBlockColor: "blue",
+    paddingLeft: 20,
+    margin: 15,
+    borderColor: "#009688",
+    backgroundColor: "white",
+    borderRadius: 10,
   },
 });
